da1f11fafccc0f8b5cc39651a71b982bdef59e9e
zeroOSeven-AI <mg.team99@gmail.com>
2026-04-14 19:06:36 +0200

    Update scraper_bild.py

--- scraper_bild.py
+++ scraper_bild.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 import os
 
 # Mapa za spremanje kropanih slika
-OUTPUT_DIR = "images"
+OUTPUT_DIR = "images_bild"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 # ============================================
